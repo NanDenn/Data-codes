fa88c55cd6486aff8f31a51c9bc92376be0923e6
Nancy Dennis <80168475+NanDenn@users.noreply.github.com>
2022-02-22 21:28:22 -0500

    first code
    First code in series

---
+++ Madlibs.py
@@ -0,0 +1,16 @@
+# string concatenation (ake hw to put strings together)
+# Suppose we want to create a string that says "subscribe to _______"
+#youtuber = "Nancy Clap" # some string variable 
+# a few ways to do this
+#print("subscribe to " + youtuber)
+#print("subscribe to {}".format(youtuber))
+#print(f"subscribe to {youtuber}")
+
+adj = input("Adjective:")
+verb1 = input("Verb: ")
+verb2 = input("verb: ")
+
+madlib = (f"Computer Programming is so {adj}! it makes me so excited \
+    to learn it. I love to {verb1}. Stay cool and {verb2}!")
+
+print(madlib)
